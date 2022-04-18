7e31a7e2d5a2af2130d15e6361ce7dbf03545a9d
Hunter Borlik <hborlik@noreply.github.com>
2022-04-18 16:17:37 -0700

    stdcall

--- ev2/src/window.cpp
+++ ev2/src/window.cpp
@@ -26,6 +26,7 @@ void glfw_error_callback(int error, const char *description)
 }
 
 // gl error callback
+__attribute__ ((stdcall))
 void gl_debug_callback(GLenum source, GLenum type, GLuint id, GLenum severity,
         GLsizei length, const GLchar *message, const void *userParam) {
     std::string output_severity{};
