e57efbae84e6fc92f6553f258ffa48e278f1fc23
Hunter Borlik <hborlik@noreply.github.com>
2022-11-08 13:19:54 -0800

    terrain.h

---
+++ ev2/include/renderer/terrain.h
@@ -0,0 +1,19 @@
+/**
+ * @file terrain.h
+ * @brief 
+ * @date 2022-11-08
+ * 
+ */
+#ifndef EV2_RENDERED_TERRAIN_H
+#define EV2_RENDERED_TERRAIN_H
+
+namespace ev2::renderer {
+
+class Terrain {
+public:
+    
+};
+
+}
+
+#endif // EV2_RENDERED_TERRAIN_H
